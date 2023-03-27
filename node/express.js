c66e0744112ce00f62db46adc89e3f644222eeee
jiazeshuai <2896613845@qq.com>
2023-03-28 00:24:04 +0800

    创建服务器，给一个初始值

--- node/express.js
+++ node/express.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-app.set("/student", (res, rej) => {});
+const obj = {
+    name: "孙悟空",
+    age: 18,
+    hobby: ["金箍棒", "猴毛"],
+};
+app.get("/student", (req, res) => {
+    res.send({
+        status: "ok",
+        data: obj,
+    });
+});
 app.listen(3000, () => {
     console.log("服务器开启");
 });
