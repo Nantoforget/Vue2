1f4396d63da2e540b8735002934277fc3c29b188
jiazeshuai <2896613845@qq.com>
2023-03-29 14:48:25 +0800

    自定义指令

--- 02-vue/src/App.vue
+++ 02-vue/src/App.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
-        <h1>我是App组件</h1>
+        <h1 v-big="msg">我是App组件</h1>
         <todo-header></todo-header>
     </div>
 </template>
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
 export default {
+    //注册组件
     components: { TodoHeader },
+    //自定义指令，没有this(局部指令)(操作真实的DOM)
+    directives: {
+        //第一个参数是指令绑定的真实的DOM节点
+        //第二个参数是指令的相关信息
+        //第三个参数是虚拟对象
+        //第四个参数是虚拟对象
+        big(element, option) {
+            console.log(option);
+        },
+    },
     // 组件名字
     name: "App",
     // 状态数据
     data() {
-        return {};
+        return {
+            msg: "abcd",
+        };
     },
     methods: {},
     computed: {},
--- 02-vue/src/main.js
+++ 02-vue/src/main.js
@@ -7,7 +7,8 @@ Vue.config.productionTip = false;
 import Carousel from "@/components/Carousel";
 //全局注册(组建的名字(name),组件的配置项)
 Vue.component("Carousel", Carousel);
-
+//全局的自定义指令
+Vue.directive("big", () => {});
 new Vue({
     render: (h) => h(App),
 }).$mount("#app");
